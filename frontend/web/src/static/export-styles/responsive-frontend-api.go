9d4482b186d489e55451f2a7225b5ae2e0f7db06
palopmy <palopmy@users.noreply.github.com>
2025-11-12 13:30:17 +0000

    Add controller-upload-download.sass, interface-components-core.styl, ape.py, responsive-frontend-api.go

---
+++ frontend/web/src/static/export-styles/responsive-frontend-api.go
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/watermark_z/frontend/web/src/static/export-styles/responsive-frontend-api.go
+// Generated at: 2025-12-03T23:45:48.612Z
+
